792a2c1111b85cb16a7482dad291f92ca26df88f
Oliver Woods <oliverwoods@Olivers-MacBook-Pro.local>
2014-11-26 16:39:29 +0000

    Adding Mr Bounce

---
+++ MrBounce/MainMenu.js
@@ -0,0 +1,29 @@
+
+BasicGame.MainMenu = function (game) {
+
+
+};
+
+BasicGame.MainMenu.prototype = {
+
+	create: function () {
+
+	    
+
+	},
+
+	update: function () {
+
+		//	Do some nice funky main menu effect here
+
+	},
+
+	resize: function (width, height) {
+
+		//	If the game container is resized this function will be called automatically.
+		//	You can use it to align sprites that should be fixed in place and other responsive display things.
+
+
+	}
+
+};
---
+++ MrBounce/Preloader.js
@@ -0,0 +1,40 @@
+
+BasicGame.Preloader = function (game) {
+
+	this.background = null;
+	this.preloadBar = null;
+
+	this.ready = false;
+
+};
+
+BasicGame.Preloader.prototype = {
+
+	preload: function () {
+
+		//	These are the assets we loaded in Boot.js
+		//	A nice sparkly background and a loading progress bar
+
+		this.background = this.add.sprite(0, 0, 'preloaderBackground');
+		this.preloadBar = this.add.sprite(300, 400, 'preloaderBar');
+
+		//	This sets the preloadBar sprite as a loader sprite.
+		//	What that does is automatically crop the sprite from 0 to full-width
+		//	as the files below are loaded in.
+
+		this.load.setPreloadSprite(this.preloadBar);
+
+		//	Here we load the rest of the assets our game needs.
+
+	    this.load.image('player', 'assets/player.png');
+	    this.load.image('ground', 'assets/ground.png');
+
+	},
+
+	create: function () {
+
+		this.state.start('Game');
+
+	}
+
+};

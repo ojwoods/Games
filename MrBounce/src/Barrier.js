ca35a0f20efa82cdf2ee8b3840b949410631aaa5
Oliver Woods <oliverwoods@Olivers-MacBook-Pro.local>
2014-12-01 10:15:23 +0000

    Missing files from last commit

---
+++ MrBounce/src/Barrier.js
@@ -0,0 +1,24 @@
+define([
+    'phaser'
+], function(Phaser) {
+    'use strict';
+
+    var Barrier = function Barrier(game, x, y, frame) {
+        Phaser.Sprite.call(this, game, x, y, 'barrier', frame);
+        this.anchor.setTo(0.5, 0.5);
+        this.game.physics.arcade.enableBody(this);
+
+        this.body.immovable = true;
+        //this.body.checkCollision.up = true;
+        //this.body.checkCollision.down = false;
+
+
+        this.checkWorldBounds = true;
+        this.outOfBoundsKill = true;
+    };
+
+    Barrier.prototype = Object.create(Phaser.Sprite.prototype);
+    Barrier.prototype.constructor = Barrier;
+
+    return Barrier;
+});
---
+++ MrBounce/src/Coin.js
@@ -0,0 +1,24 @@
+define([
+    'phaser'
+], function(Phaser) {
+    'use strict';
+
+    var Coin = function Coin(game, x, y, frame) {
+        Phaser.Sprite.call(this, game, x, y, 'coin', frame);
+        this.anchor.setTo(0.5, 0.5);
+        this.game.physics.arcade.enableBody(this);
+
+        this.body.immovable = true;
+        //this.body.checkCollision.up = true;
+        //this.body.checkCollision.down = false;
+
+
+        this.checkWorldBounds = true;
+        this.outOfBoundsKill = true;
+    };
+
+    Coin.prototype = Object.create(Phaser.Sprite.prototype);
+    Coin.prototype.constructor = Coin;
+
+    return Coin;
+});
